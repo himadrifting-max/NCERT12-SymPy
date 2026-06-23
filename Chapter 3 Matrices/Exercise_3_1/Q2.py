4ec1c2c023eb6790e12b5941559d265921a6a304
DragonLordHimadri <himadrifting@gmail.com>
2026-06-23 21:20:47 +0530

    Add Exercise 3.1 Question 2 solution

---
+++ Chapter 3 Matrices/Exercise_3_1/Q2.py
@@ -0,0 +1,19 @@
+import sympy as sp 
+import numpy as np
+from sympy import symbols, Matrix, Rational, sqrt
+
+x = sp.symbols('x')
+
+#Question 2
+# If a matrix has 24 elements, what are the possible orders it can have ? what if it has 13 elements ?
+
+def find_orders(num_elements):
+    orders = []
+    for i in range(1, num_elements + 1):
+        if num_elements % i == 0:
+            j = num_elements // i
+            orders.append((i, j))
+    return orders
+
+print("Possible orders for 24 elements:", find_orders(24))
+print("Possible orders for 13 elements:", find_orders(13))
